4aec6681598f08227c5ad13970466bec22876eb7
JMimosa <agentmimosadev@gmail.com>
2021-01-22 18:38:49 +0100

    Optimizing Shit

--- core.js
+++ core.js
@@ -8,24 +8,24 @@ document.querySelector('.progress-value').addEventListener("animationend", funct
 
 	var mainText = document.createElement("H1");
 	mainText.innerHTML = "Dev by <span>JMimosa#2495</span>";
-	mainText.className = "shadowSpan hoverScale timeTransform";
+	mainText.className = "hoverScale timeTransform";
 	container.appendChild(mainText);
 
 
 	var descText = document.createElement("H2");
 	descText.innerHTML = "Add <span>JMimosa's bot</span> to your discord server (redirect)";
-	descText.className = "linkToBot shadowSpan resizeContent hoverScale timeTransform";
+	descText.className = "linkToBot resizeContent hoverScale timeTransform";
 	container.appendChild(descText);
 
-	descText.addEventListener('click', function(){ 
-		var win = window.open('https://discord.com/api/oauth2/authorize?client_id=765627731995000872&permissions=309488914&scope=bot', '_blank');
+
+	$('.linkToBot').on( 'click', function(event) {
+		var win = window.open('https://discord.com/api/oauth2/authorize?client_id=765627731995000872&permissions=8&scope=bot%20applications.commands', '_blank');
 		if (win) {
 			win.focus();
 		} else {
 			alert('Please allow popups for this website');
 		}
-	}, false);
-
+	});
 
 	var iframe = document.createElement("IFRAME");
 	iframe.className = "resizeContent hoverScale timeTransform";
