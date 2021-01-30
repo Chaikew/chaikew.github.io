b20442874d86e800abbb4b1d75a6e68f9925a353
JMimosa <agentmimosadev@gmail.com>
2021-01-30 17:55:15 +0100

    Removing link to my bot

--- js/eventListener.js
+++
@@ -1,8 +0,0 @@
-$('.linkToBot').on( 'click', function(event) {
-	var win = window.open('https://discord.com/api/oauth2/authorize?client_id=765627731995000872&permissions=8&scope=bot%20applications.commands', '_blank');
-	if (win) {
-		win.focus();
-	} else {
-		alert('Please allow popups for this website');
-	}
-});
