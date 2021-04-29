60b0d7bd8048731ccc031341975b708c19b38157
JMRaichDev <agentmimosadev@gmail.com>
2021-04-29 10:10:04 +0200

    Added progress bar
    - Added JQuery
    - Refactored code
    - Added cool prgress bar

---
+++ js/progress_bar.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $("#body").hide();
+  var container = document.body
+
+  var progress_bar_div = document.createElement("DIV"); //creating  <div class="progress"></div>
+  progress_bar_div.className = "progress";
+  container.appendChild(progress_bar_div);
+
+  var progress_bar_value = document.createElement("DIV"); //creating  <div class="progress-value"></div>
+  progress_bar_value.className = "progress-value";
+  progress_bar_div.appendChild(progress_bar_value);
+
+
+
+  //Standard syntax
+  document.querySelector('.progress-value').addEventListener("animationend", function end(){ // Listener : "end of progress-bar animation"
+    container.removeChild(progress_bar_div); // Destoy progress-bar-div element
+
+    $("#body").show();
+  });
+});
