ac009da63ad74a9e0f6d3e196f5baccecc41069f
JMRaichDev <agentmimosadev@gmail.com>
2021-06-06 18:25:03 +0200

    Added RAINBOOOW !!!

--- js/progress_bar.js
+++ js/progress_bar.js
@@ -3,19 +3,18 @@ $(document).ready(function(){
   var container = document.body
 
   var progress_bar_div = document.createElement("DIV"); //creating  <div class="progress"></div>
-  progress_bar_div.className = "progress";
+  progress_bar_div.id = "progress";
   container.appendChild(progress_bar_div);
 
   var progress_bar_value = document.createElement("DIV"); //creating  <div class="progress-value"></div>
-  progress_bar_value.className = "progress-value";
+  progress_bar_value.id = "progress-value";
   progress_bar_div.appendChild(progress_bar_value);
 
 
 
   //Standard syntax
-  document.querySelector('.progress-value').addEventListener("animationend", function end(){ // Listener : "end of progress-bar animation"
+  document.querySelector('#progress-value').addEventListener("animationend", function end(){ // Listener : "end of progress-bar animation"
     container.removeChild(progress_bar_div); // Destoy progress-bar-div element
-
     $("#body").show();
   });
 });
