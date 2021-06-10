da1ef4fc504068b5b3326d35bb200323e64520c1
JMRaichDev <agentmimosadev@gmail.com>
2021-06-10 21:38:39 +0200

    Added glitch effect

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+function randomIntFromInterval(min, max) { // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 $(document).ready(function(){
   $("#body").hide();
   var container = document.body
@@ -16,5 +20,13 @@ $(document).ready(function(){
   document.querySelector('#progress-value').addEventListener("animationend", function end(){ // Listener : "end of progress-bar animation"
     container.removeChild(progress_bar_div); // Destoy progress-bar-div element
     $("#body").show();
+  });
+
+  document.querySelector('#body').addEventListener("animationend", function end(){ // Listener : "end of progress-bar animation"
+    var target_glicthes = document.getElementsByClassName('glitch')
+    for (i = 0; i < target_glicthes.length; i++) {
+      glitch_instances[i] = new Glitch(target_glicthes[i]);
+    }
+    requestAnimationFrame(start_glitch);
   });
 });
