754b762b22aeff347e5b6c704cd723c157c79c26
JMimosa <agentmimosadev@gmail.com>
2021-07-02 10:35:54 +0200

    Removed "JustForFun" code

--- js/main.js
+++ js/main.js
@@ -9,57 +9,18 @@ function checkIE() {
     }
 }
 
-function checkIp() {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://httpbin.org/ip");
-    xhr.send();
-    xhr.onload = function() {
-        if (xhr.readyState === xhr.DONE) {
-            if (xhr.status === 200) {
-                var md5Hash = MD5(JSON.parse(xhr.responseText).origin.toString());
-                lastIpHash = localStorage.getItem("lastip")
-                if (lastIpHash != null) {
-                    if (lastIpHash != md5Hash) {
-                        alert("Warning ! Your ip has changed !");
-                        localStorage.setItem("lastip", md5Hash);
-                    }
-                } else {
-                    localStorage.setItem("lastip", md5Hash);
-                }
-            }
-        }
-    };
-}
-
-function checkIframe() {
-    var isInIframe;
-    try {
-        isInIframe = (window.self !== window.top);
-    } catch (e) {
-        isInIframe = true;
+/* Function to handle Left-Side menu */
+function updateNav() {
+    let sidenavMenu =  _("#sidenavMenu");
+    if (sidenavMenu.style.width == "260px") {
+        sidenavMenu.css("width", "0px");
+    } else {
+        sidenavMenu.css("width", "260px");
     }
-    if (isInIframe) {
-        document.open();
-        document.close();
-
-        document.title = "Error"
-        document.body.innerHTML = "<p>[SECURITY] Page loaded in IFRAME<br>[SECURITY] Don't trust anything here !</p>";
-    }
-}
-
-/* Functions to handle Left-Side menu */
-function openNav() {
-    document.getElementById("sidenavMenu").style.width = "260px";
-}
-
-function closeNav() {
-    document.getElementById("sidenavMenu").style.width = "0";
 }
 
 document.onready(function() {
     checkIE();
-    checkIframe();
-    checkIp();
     _("#body").css("visibility", "hidden");
 
     //Standard syntax
