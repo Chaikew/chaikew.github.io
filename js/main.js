1bc25743401fc2588cc209ce968a905c273149c9
JMRaichDev <agentmimosadev@gmail.com>
2021-08-22 16:03:02 +0200

    Fixing

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-interface Object {
-    onready(fn): any;
-}
-
 document.onready = function (fn) {
     // see if DOM is already available
     if (document.readyState === "complete" || document.readyState === "interactive") {
@@ -14,7 +10,7 @@ document.onready = function (fn) {
 
 document.onready(function() {
     // check if browser is IE
-    let isIE:boolean = /MSIE|Triden/.test(navigator.userAgent);
+    let isIE = /MSIE|Triden/.test(navigator.userAgent);
     if (isIE) {
         // clear document
         document.open();
